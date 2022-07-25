01f4b4788a1107b0c8abda50adaa35335b70e05f
N3cker <piaszuu@gmail.com>
2022-07-26 00:18:23 +0200

    Finish tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWrapper = document.querySelector('.gallery');
+
+
+const imageCreate = (src, data, alt) => {
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    div.classList.add('gallery__item');
+
+    a.classList.add('gallery__link');
+    a.href = data;
+
+    img.classList.add('gallery__image');
+    img.src = src;
+    img.setAttribute('data-source', data);
+    img.alt = alt;
+    a.appendChild(img);
+    div.appendChild(a);
+    return div;
+}
+
+const addImagesToGallery = () => {
+    galleryItems.forEach(item => {
+        const img = imageCreate(item.preview, item.original, item.description);
+        galleryWrapper.appendChild(img);
+    })
+}
+
+addImagesToGallery();
+
+galleryWrapper.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') return;
+    const instance = basicLightbox.create(`<img src="${event.target.getAttribute('data-source')}" width="800" height="600">`)
+    instance.show();
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryWrapper = document.querySelector('.gallery');
+
+const imageCreate = (src, data, alt) => {
+    
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    a.classList.add('gallery__item');
+    a.href = data;
+
+    img.classList.add('gallery__image');
+    img.src = src;
+    img.alt = alt;
+    img.title = alt;
+    a.appendChild(img);
+
+    return a;
+}
+
+const addImagesToGallery = () => {
+    galleryItems.forEach(item => {
+        const img = imageCreate(item.preview, item.original, item.description);
+        galleryWrapper.appendChild(img);
+    })
+}
+
+addImagesToGallery();
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
